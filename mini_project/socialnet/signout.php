fd5c0996731cfd8de3f452ec81197c5953d61d3e
lybahoang <157574648+lybahoang@users.noreply.github.com>
2026-04-25 16:21:41 +0700

    modify signout.php to destroy the session and redirect to homepage.php

--- mini_project/socialnet/signout.php
+++ mini_project/socialnet/signout.php
@@ -1,1 +1,14 @@
-This is signout page.
+<?php
+// Resume the session.
+session_start();
+
+// Unset all session variables.
+session_unset();
+
+// Destroy the session.
+session_destroy();
+
+// Redirect to homepage.php.
+header("Location: homepage.php");
+exit();
+?>
