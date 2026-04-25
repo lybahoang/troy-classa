3302bd023fc80fe8d53411ba93ad319a127d669a
lybahoang <157574648+lybahoang@users.noreply.github.com>
2026-04-25 12:33:58 +0700

    add new_user.php - form input and form handling in a file

---
+++ mini_project/admin_area/new_user.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+
+<h2>Create Account</h2>
+
+<form action="/troy-classa/mini_project/admin_area/new_user.php" method="POST">
+    <label>Username:</label>
+    <input type="text" name="username" required><br><br>
+
+    <label>Full Name:</label>
+    <input type="text" name="fullname" required><br><br>
+
+    <label>Password:</label>
+    <input type="text" name="password" required><br><br>
+
+    <label>Description:</label><br>
+    <textarea name="description"></textarea><br><br>
+
+    <button type="submit">Register</button>
+</form>
+</body>
+</html>
