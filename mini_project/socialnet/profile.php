1b32dcc69f4ac9fdc50fcd988af059ff0c10c7b1
lybahoang <157574648+lybahoang@users.noreply.github.com>
2026-04-25 16:42:49 +0700

    modify profile.php to display the user fullname and description

--- mini_project/socialnet/profile.php
+++ mini_project/socialnet/profile.php
@@ -2,6 +2,31 @@
 
 <?php
 session_start();
-
-
+require_once("db.php");
 ?>
+
+<?php
+// Get the user fullname and description.
+$fullname = "";
+$description = "";
+
+if (!isset($_SESSION['username'])) {
+    header("Location: signin.php");
+    exit();
+} else {
+    $result = db_query("SELECT fullname, description FROM accounts WHERE username = '" . $_SESSION['username'] . "'");
+    if (count($result) > 0) {
+        $fullname = $result[0]['fullname'];
+        $description = $result[0]['description'];
+    }
+}
+?>
+
+<html lang="en">
+    <body>
+        <h1>Profile: <?= $_SESSION['username'] ?></h1>
+        <div><p><strong>Fullname: </strong> <?= $fullname ?></p></div>
+        <div><p><strong>Description: </strong> <?= $fullname ?></p></div>
+        <?= $description ?>
+    </body>
+</html>
